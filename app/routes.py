41f659c11c04d3ce8c5863c91e2d1d11eceb2738
Guilherme Telles <guilhermetelles890@gmail.com>
2026-08-31 19:52:35 -0300

    Criação de Tela "Cadastrar"
    Criação da tela para realizar o cadastro de novos usuários

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,12 @@ from flask import render_template
 
 from app import app
 
+
 @app.route("/")
 def inicio():
     return render_template("index.html")
+
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
