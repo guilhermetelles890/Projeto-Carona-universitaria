d061d8affef97a26279f2d0c630e09f564c89723
Matheus Agum <mathues.fagum@gmail.com>
2026-09-03 11:58:10 -0300

    Criação e testes com banco de dados, aparentemente está salvando, validar isso mais para frente.

--- run.py
+++ run.py
@@ -1,4 +1,18 @@
 from app import app
+import sqlite3
+
+conexao = sqlite3.connect('banco.db')
+cursor = conexao.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nome TEXT NOT NULL
+                )''')
+
+cursor.execute('''INSERT INTO usuarios (nome) VALUES ('Matheus')''')
+
+conexao.commit()
+conexao.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
