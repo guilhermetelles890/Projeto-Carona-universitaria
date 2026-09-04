4d7a0055cd1ee969e692678a225adfceda6fd729
Guilherme Telles <guilhermetelles890@gmail.com>
2026-09-04 19:41:00 -0300

    Ajustes no banco de dados
    Separação do banco de dados e run, criando um arquivo para o mesmo

---
+++ app/database.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+CAMINHO_BANCO = "database/banco.db"
+
+
+def conectar_banco():
+    conexao = sqlite3.connect(CAMINHO_BANCO)
+    conexao.row_factory = sqlite3.Row
+
+    return conexao
+
+
+def criar_tabela_usuarios():
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            senha TEXT NOT NULL,
+            telefone TEXT NOT NULL,
+            curso TEXT NOT NULL,
+            campus TEXT NOT NULL,
+            tipo_usuario TEXT NOT NULL,
+            data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+    conexao.commit()
+    conexao.close()
+
+
+def cadastrar_usuario(nome, email, senha, telefone, curso, campus, tipo_usuario):
+    conexao = conectar_banco()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO usuarios
+        (nome, email, senha, telefone, curso, campus, tipo_usuario)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """, (nome, email, senha, telefone, curso, campus, tipo_usuario))
+
+    conexao.commit()
+    conexao.close()
+
+
+criar_tabela_usuarios()
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 from app import app
+from app.database import cadastrar_usuario
+from werkzeug.security import generate_password_hash
 
 
 @app.route("/")
@@ -8,8 +10,31 @@ def inicio():
     return render_template("index.html")
 
 
-@app.route("/cadastro")
+@app.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        senha = request.form["senha"]
+        telefone = request.form["telefone"]
+        curso = request.form["curso"]
+        campus = request.form["campus"]
+        tipo_usuario = request.form["tipo_usuario"]
+
+        senha_hash = generate_password_hash(senha)
+
+        cadastrar_usuario(
+            nome,
+            email,
+            senha_hash,
+            telefone,
+            curso,
+            campus,
+            tipo_usuario
+        )
+
+        return redirect(url_for("login"))
+
     return render_template("cadastro.html")
 
 
